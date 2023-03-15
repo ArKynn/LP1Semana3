5a78b41efc6935cf3a2341c4f319406bc4163e9f
David Mendes <david.f.mendes@proton.me>
2023-03-15 13:52:14 +0000

    Asks for a string and a character and returns the string without the character provided

---
+++ ChangeString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert the string to modify:");
+            string phrase = Console.ReadLine();
+            Console.WriteLine("Insert the character to remove:"); 
+            char character = Convert.ToChar(Console.ReadLine());
+
+            string resultingString = "";
+
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                if (phrase[i] != character)
+                {
+                    resultingString += phrase[i];
+                }
+                else
+                {
+                }
+                
+            }
+            Console.WriteLine($"Resulting string: {resultingString}");
+        }
+    }
+}
