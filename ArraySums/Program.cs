87c3b440902636c0c47b6c6682f4d6f9d659876b
David Mendes <david.f.mendes@proton.me>
2023-03-15 14:06:30 +0000

    Added array size and individual elements input

--- ArraySums/Program.cs
+++ ArraySums/Program.cs
@@ -6,7 +6,22 @@ namespace ArraySums
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Insert the horizontal size of the array:");
+            int horizontalSize = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Insert the vertical size of the array:");
+            int verticalSize = Convert.ToInt16(Console.ReadLine());
+
+            int[,] matrix = new int[horizontalSize, verticalSize];
+
+            for (int i = 0; i < verticalSize; i++)
+            {
+                for (int j = 0; j < horizontalSize; j++)
+                {
+                    Console.WriteLine($"Current array element: {j},{i} \nInsert element value:");
+                    matrix[j, i] = Convert.ToInt16(Console.ReadLine());
+                }
+            }
+            
         }
     }
 }
