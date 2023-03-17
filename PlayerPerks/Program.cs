f2187ecfa106a54c4ad5e266ef526ccc9c816191
David Mendes <david.f.mendes@proton.me>
2023-03-17 18:30:57 +0000

    Reads string from argument and prints string based on characters present in string

--- PlayerPerks/Program.cs
+++ PlayerPerks/Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 
 namespace PlayerPerks
 {
     class Program
     {
+        [Flags]
+        enum Perks
+        {
+            None,
+            WaterBreathing,
+            Stealth,
+            AutoHeal,
+            DoubleJump
+        };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                string inputPerks = args[0];
+                Perks playerWaterBreathing = Perks.None;
+                Perks playerStealth = Perks.None;
+                Perks playerAutoHeal = Perks.None;
+                Perks playerDoubleJump = Perks.None;
+                
+                for (int c = 0; c < inputPerks.Length; c++)
+                {
+                    if (inputPerks[c] == Convert.ToChar("w"))
+                    {
+                        playerWaterBreathing ^= Perks.WaterBreathing;
+                    }
+                    else if (inputPerks[c] == Convert.ToChar("a"))
+                    {
+                        playerAutoHeal ^= Perks.AutoHeal;
+                    }
+                    else if (inputPerks[c] == Convert.ToChar("s"))
+                    {
+                        playerStealth ^= Perks.Stealth;
+                    }
+                    else if (inputPerks[c] == Convert.ToChar("d"))
+                    {
+                        playerDoubleJump ^= Perks.DoubleJump;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unknown perk!");
+                        Environment.Exit(0);
+                    }
+                }
+
+                Console.WriteLine($"{playerWaterBreathing}, {playerStealth}, {playerAutoHeal}, {playerDoubleJump}");
+                
+                if (playerStealth == Perks.Stealth && playerDoubleJump == Perks.DoubleJump)
+                {
+                    Console.WriteLine("Silent Jumper!");
+                }
+
+                if (playerAutoHeal != Perks.AutoHeal)
+                {
+                    Console.WriteLine("Not gonna make it!");
+                }
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                Console.WriteLine("No Perks at all!");
+                Environment.Exit(0);
+            }
         }
     }
-}
+}    
