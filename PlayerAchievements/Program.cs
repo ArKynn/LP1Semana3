3b612a01188858f55609424eddb161073cdc9d0f
David Mendes <david.f.mendes@proton.me>
2023-03-17 13:50:16 +0000

    Renamed Project, added input for number of players and each player's owned achievements, and print of those same achievements.

---
+++ PlayerAchievements/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace PlayerAchivements
+{
+    class Program
+    {
+        enum Achievements
+        {
+            DefeatOptionalBoss,
+            FindHiddenLevel,
+            FinishGame
+        };
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert number of players:");
+            int n = Convert.ToInt16(Console.ReadLine());
+
+            int[,] playerAchievements = new int[n, 3];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"Does player {i + 1} have the {(Achievements)j} Achievement? (Y = 1/N = 0)");
+                    playerAchievements[i, j] = Convert.ToInt16(Console.ReadLine());
+                }
+            }
+            
+            for (int i = 0; i < n; i++)
+            { 
+                Console.WriteLine($"Player {i +1 }'s achievements:");
+
+                int achievementsOwned = 0;
+                
+                for (int j = 0; j < 3; j++)
+                {
+                    if (playerAchievements[i, j] == 1)
+                    {
+                        Console.WriteLine($"{(Achievements) j}");
+                        achievementsOwned++;
+                    }
+
+                    if (achievementsOwned == 3)
+                    {
+                        Console.WriteLine("Completionist!");
+                    }
+                }
+                Console.WriteLine($"\n");
+            }
+        }
+    }
+}
