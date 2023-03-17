1d8518565215df1acd8dd2dec553bf86e844df2e
David Mendes <david.f.mendes@proton.me>
2023-03-17 13:03:36 +0000

    Created PlayerAchievements project

---
+++ PlayerAchivements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayerAchivements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
